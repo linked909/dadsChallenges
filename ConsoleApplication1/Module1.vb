625842921f3e69590eba8bd9bc2e20b920537c85
linked909 <jamespfarnell@gmail.com>
2016-01-02 16:03:47 +0000

    James First Commit. Fizz Buzz First Attempt.

---
+++ ConsoleApplication1/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim number As Integer
+        Dim counter As Integer
+
+        Console.WriteLine("Welcome to FizzBuzz")
+        Console.WriteLine("Pick any Number")
+        number = Console.ReadLine()
+
+        For counter = 1 To number
+            If number / counter = 3 Then
+                Console.Write("Fizz")
+            End If
+        Next
+
+        For counter = 1 To number
+            If number / counter = 5 Then
+                Console.WriteLine("Buzz")
+            End If
+        Next
+
+
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
