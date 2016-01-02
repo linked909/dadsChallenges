f3a4971bf93072caafc425eabdd42753ec1f12aa
linked909 <jamespfarnell@gmail.com>
2016-01-02 16:20:53 +0000

    FizzBuzz v2 Added ability to differentiate numeric values from strings

--- ConsoleApplication1/Module1.vb
+++ ConsoleApplication1/Module1.vb
@@ -1,12 +1,22 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim number As Integer
+        Dim number As String
         Dim counter As Integer
+        'this is a comment
 
-        Console.WriteLine("Welcome to FizzBuzz")
-        Console.WriteLine("Pick any Number")
-        number = Console.ReadLine()
+        Do
+
+            Console.WriteLine("Welcome to FizzBuzz")
+            Console.WriteLine("Pick any Number")
+            number = Console.ReadLine()
+
+            If IsNumeric(number) = False Then
+                Console.WriteLine("Try Again...")
+                Console.WriteLine()
+            End If
+
+        Loop While IsNumeric(number) = False
 
         For counter = 1 To number
             If number / counter = 3 Then
